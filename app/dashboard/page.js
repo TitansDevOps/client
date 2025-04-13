8a2af91eba32be3f26d6ffa9598beb546fe6038e
maicolaa <maicolaroyave10@gmail.com>
2025-04-12 20:20:23 -0500

    feat: sidebar into adoption-centers

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,37 +1,13 @@
 "use client";
 
-import { SidebarItem } from './components/Sidebar'
-import Sidebar from './components/Sidebar'
-import {
-  MessageCircleQuestion,
-  Receipt,
-  Boxes,
-  Package,
-  UserCircle,
-  BarChart3,
-  LayoutDashboard,
-  Settings,
-} from "lucide-react"
+import ProtectedRoute from "@/app/context/ProtectedRoute";
+import SidebarDashboard from "@/app/components/Sidebar";
 
 export default function App() {
   return (
-    <main className="App">
-      <Sidebar>
-      <div className="mt-4">
-      <SidebarItem
-        icon={<LayoutDashboard size={20} style={{color:"black"}}/>}
-        text={<span style={{ color: "black" }}>Dashboard</span>}       
-        />
-        <SidebarItem icon={<BarChart3 size={20} style={{color:"black"}}/>} text={<span style={{ color: "black" }}>Statistics</span>}/>
-        <SidebarItem icon={<UserCircle size={20} style={{color:"black"}}/>} text={<span style={{ color: "black" }}>Users</span>}/>
-        <SidebarItem icon={<Boxes size={20} style={{color:"black"}}/>} text={<span style={{ color: "black" }}>Inventory</span>}/>
-        <SidebarItem icon={<Package size={20} style={{color:"black"}}/>} text={<span style={{ color: "black" }}>Orders</span>}/>
-        <SidebarItem icon={<Receipt size={20} style={{color:"black"}}/>} text={<span style={{ color: "black" }}>Billings</span>}/>
-        <hr className="my-3" />
-        <SidebarItem icon={<Settings size={20} style={{color:"black"}}/>} text={<span style={{ color: "black" }}>Settings</span>}/>
-        <SidebarItem icon={<MessageCircleQuestion size={20} style={{color:"black"}}/>} text={<span style={{ color: "black" }}>Help</span>}/>
-        </div>
-      </Sidebar>
-    </main>
-  )
+    // <ProtectedRoute>
+    <SidebarDashboard />
+
+    // </ProtectedRoute>
+  );
 }
